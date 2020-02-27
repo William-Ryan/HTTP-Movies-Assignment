b1f036d3d9a90b9cd99c3745e331101f56c17e02
William Ryan <billy.ryan.rom@gmail.com>
2020-02-27 12:02:18 -0500

    Add UpdateMovie to App.js/Remove Get Request/Refractor State

--- client/src/App.js
+++ client/src/App.js
@@ -3,40 +3,32 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
-import axios from 'axios';
+import UpdateMovie from "./Movies/UpdateMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
-  const [movieList, setMovieList] = useState([]);
-
-  const getMovieList = () => {
-    axios
-      .get("http://localhost:5000/api/movies")
-      .then(res => setMovieList(res.data))
-      .catch(err => console.log(err.response));
-  };
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
 
-  useEffect(() => {
-    getMovieList();
-  }, []);
-
   return (
-    <>
+    <div>
       <SavedList list={savedList} />
+      <Route exact path="/" component={MovieList} />
+      <Route
+        path="/movies/:id"
+        render={props => {
+          return <Movie {...props} addToSavedList={addToSavedList} />;
 
-      <Route exact path="/">
-        <MovieList movies={movieList} />
-      </Route>
-
-      <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
-      </Route>
-    </>
-  );
-};
-
-export default App;
+        }}/>
+        <Route 
+          path="/update-movie/:id"
+          render={props => {
+            return <UpdateMovie {...props} addToSavedList={addToSavedList} />;
+          }}/>
+      </div>
+    );
+  };
+  
+  export default App;
